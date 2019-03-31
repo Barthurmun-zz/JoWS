aee23dae44260a6235adb1dd0de91166bbedccb3
Barthurmun <kuba.bryl1996@gmail.com>
2019-03-31 18:10:40 -0400

    UL

--- proj_1.py
+++ proj_1.py
@@ -21,6 +21,7 @@ def topology():
     
     sta1 = net.addStation('sta1', mac='00:00:00:00:00:01',position='20,8,0') #YouTube
     sta2 = net.addStation('sta2', mac='00:00:00:00:00:02',position='20,7,8') #YouTube
+    #sta2 = net.addHost('sta2', mac='00:00:00:00:00:02',position='20,7,8') #YouTube
     
     ap1 = net.addAccessPoint('ap1', ssid='Jows-proj1', channel='36', mode='ac', position='25,10,15')
         
@@ -55,16 +56,31 @@ def topology():
     
     #Pewnie trzeba dodac tutaj odpowiednie Wait'y
     sleep(5)
-    ap1.cmd("tc qdisc add dev ap1-eth2 root handle 1: htb default 13")
+    ap1.cmd("tc qdisc add dev ap1-eth2 root handle 1: htb default 90")
     ap1.cmd("tc class add dev ap1-eth2 parent 1: classid 1:1 htb rate 250kbps ceil 250kbps")
     ap1.cmd("tc class add dev ap1-eth2 parent 1:1 classid 1:10 htb rate 90kbps ceil 250kbps") #Sta1 Video
-    ap1.cmd("tc class add dev ap1-eth2 parent 1:1 classid 1:11 htb rate 90kbps ceil 250kbps") #Sta2 Video
+    #Zakomentowac ponisza linijke gry robimy DL video:
+    ap1.cmd("tc class add dev ap1-eth2 parent 1:1 classid 1:11 htb rate 124kbps ceil 250kbps") #Sta2 Video
     ap1.cmd("tc class add dev ap1-eth2 parent 1:1 classid 1:12 htb rate 1kbps ceil 250kbps") #Sta3 BE
-    ap1.cmd("tc class add dev ap1-eth2 parent 1:1 classid 1:13 htb rate 69kbps ceil 250kbps") #Sta4 VoIP
+    ap1.cmd("tc class add dev ap1-eth2 parent 1:1 classid 1:13 htb rate 35kbps ceil 250kbps") #Sta4 VoIP
+    
+
+    #Konfiguracja portu ingressowego dla VLC ( sta2 obsluguje vlc)
+    #Odkomentowa jak robimy DL
+    #ap1.cmd("modprobe ifb")
+    #ap1.cmd("ip link set dev ifb0 down")
+    #ap1.cmd("ip link set dev ifb0 up")
+    #ap1.cmd("tc qdisc add dev ifb0 root handle 1: htb default 10")
+    #ap1.cmd("tc qdisc add dev ap1-eth2 ingress handle ffff:")
+    #ap1.cmd("tc class add dev ifb0 parent 1: classid 1:1 htb rate 250kbps ceil 250kbps")
+    #ap1.cmd("tc class add dev ifb0 parent 1:1 classid 1:11 htb rate 90kbps ceil 250kbps")
+    #ap1.cmd("tc filter add dev ap1-eth2 protocol ip parent 1:0 prio 1 u32 match ip src 10.0.0.1 match ip dport 5005 0xffff flowid 1:11")
+ 
+    #ap1.cmd("tc filter add dev ap1-eth2 parent ffff: protocol ip u32 match u32 0 0 action connmark action mirred egress redirect dev ifb0 flowid ffff:1")
     
     sleep(0.5)
     ap1.cmd("tc filter add dev ap1-eth2 protocol ip parent 1:0 prio 1 u32 match ip src 10.0.0.2 match ip dport 50 0xffff flowid 1:10")
-    ap1.cmd("tc filter add dev ap1-eth2 protocol ip parent 1:0 prio 1 u32 match ip src 10.0.0.3 match ip dport 60 0xffff flowid 1:11")
+    ap1.cmd("tc filter add dev ap1-eth2 protocol ip parent 1:0 prio 1 u32 match ip src 10.0.0.3 match ip dport 5005 0xffff flowid 1:11")
     ap1.cmd("tc filter add dev ap1-eth2 protocol ip parent 1:0 prio 2 u32 match ip src 10.0.0.4 match ip dport 80 0xffff flowid 1:12")
     ap1.cmd("tc filter add dev ap1-eth2 protocol ip parent 1:0 prio 3 u32 match ip src 10.0.0.5 match ip dport 25 0xffff flowid 1:13")
 
@@ -72,29 +88,34 @@ def topology():
     #Pfifo/FIFO/SFQ
 
     sleep(0.5)
-    ap1.cmd("tc qdisc add dev ap1-eth2 parent 1:10 handle 10: pfifo limit 5")  
-    ap1.cmd("tc qdisc add dev ap1-eth2 parent 1:11 handle 20: pfifo limit 5")
+    ap1.cmd("tc qdisc add dev ap1-eth2 parent 1:10 handle 20: pfifo limit 5")  
+    ap1.cmd("tc qdisc add dev ap1-eth2 parent 1:11 handle 10: pfifo limit 15")
+    #Odkomentowac gdy robimy DL i zakomentowac linijke powyzej
+    #ap1.cmd("tc qdisc add dev ifb0 parent 1:11 handle 20: pfifo limit 5")
     ap1.cmd("tc qdisc add dev ap1-eth2 parent 1:12 handle 30: pfifo limit 5")
     ap1.cmd("tc qdisc add dev ap1-eth2 parent 1:13 handle 40: sfq perturb 10")
 
-      #Uruchamianie iperfa 
+    #Uruchamianie iperfa 
     sleep(0.5)
     internet.cmd("iperf -s -u -p 50 -i 1 > internet_log1.txt &")
-    internet.cmd("iperf -s -u -p 60 -i 1 > internet_log2.txt &")
+    #internet.cmd("iperf -s -u -p 5005 -i 1 > internet_log2.txt &")
     internet.cmd("iperf -s -p 80 -i 1 > internet_log3.txt &")
     internet.cmd("iperf -s -u -p 25 -i 1 > internet_log4.txt &")
 
     internet.cmd("tcpdump -i internet-eth0 -w jows-1.pcap &")
 
-    sta1.cmd("iperf -c 10.0.0.1 -p 50 -t 20 -u -b 2.5M &")
+    sta1.cmd("iperf -c 10.0.0.1 -p 50 -t 100 -u -b 2.5M &")
     sleep(5)
-    sta2.cmd("iperf -c 10.0.0.1 -p 60 -t 15 -u -b 2.5M &")
+    #sta2.cmd("iperf -c 10.0.0.1 -p 60 -t 55 -u -b 2.5M &")
+    #sleep(5)
+    sta3.cmd("iperf -c 10.0.0.1 -p 80 -t 140 &")
     sleep(5)
-    sta3.cmd("iperf -c 10.0.0.1 -p 80 -t 10 &")
+    sta4.cmd("iperf -c 10.0.0.1 -p 25 -t 800 -u -b 2.5M -l 160 &") #dlugosc pakietu 160 Bajtow
+    #sleep(5)
+    sta2.cmd("vlc-wrapper &")
     sleep(5)
-    sta4.cmd("iperf -c 10.0.0.1 -p 25 -t 5 -u -b 2.5M -l 160 &") #dlugosc pakietu 160 Bajtow
-    sleep(5)
-    
+    internet.cmd("vlc-wrapper &")
+    sleep(160)
     internet.cmd("sudo killall tcpdump")
 
     info("*** Running CLI\n")
